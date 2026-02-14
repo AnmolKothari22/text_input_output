4da6d04c4b2e73ecb9ed1c051d39f29b9a61dfce
Anmol Kothari <anmolkothari22@gmail.com>
2026-02-14 14:32:16 +0530

    intial commit

---
+++ executer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo -e "\033[0;31mTERMINATED\033[0m"
+	echo "give arguments please..."
+	echo "Usage: ./executer <filename>.cpp"
+    	exit 1
+fi
+
+len1=${#1}
+if [ "$len1" -lt 5 ];then
+	echo -e "\033[0;31mNOT EXECUTING , give correct filename\033[0m"
+	exit 1
+fi
+
+FILE="$1"
+TILL=$(($len1-4))
+FILENAME="${FILE:0:$TILL}"
+len2=${#FILENAME}
+#echo $FILENAME
+EXTENSION="${FILE:$((len2)):len1}"
+
+#echo $EXTENSION
+
+if [ $EXTENSION != ".cpp" ];then
+	echo -e "\033[0;31mIncorrect extention\033[0m"
+	echo "your extension was $EXTENSION but this program only works for .cpp"
+	exit 1
+fi
+
+g++ "$FILE" -o "$FILENAME".exe
+
+./"$FILENAME".exe < inputer.txt > outer.txt
+
+
