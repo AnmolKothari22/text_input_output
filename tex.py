e55d63869ee3db3c89fcf5ee4b24146b53433f52
Anmol Kothari <anmolkothari22@gmail.com>
2026-02-20 21:48:44 +0530

    python script for selecting testcases

---
+++ tex.py
@@ -0,0 +1,30 @@
+import sys
+s=set()
+
+for a in sys.argv:
+    s.add(a)
+#print(s)
+arr = [str(len(sys.argv)-1)+"\n"]
+try:
+    temp=""
+    with open("inputer.txt", "r", encoding="utf-8") as file:
+        t = 0
+        for line in file:
+         #   print(t,temp)
+            if (len(line)==1):
+                if((t>0) and (str(t) in s)):
+                    arr.append(temp)
+                temp=""
+                t=t+1
+            else:
+              #  print(line,end="")
+                temp=temp+line
+    if(t in s):
+        arr.append(temp)
+    for ele in arr:
+        print(ele,end="")
+                
+except Exception as e:
+    print("Error:", e)
+
+
